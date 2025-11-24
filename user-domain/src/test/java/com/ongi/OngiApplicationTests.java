6b3c82fa0fe8f36d4418733d915801a97da77fb4
Seungmin123 <dltmdals147@gmail.com>
2025-11-24 16:29:01 +0900

    Initial commit: Spring Boot project setup

---
+++ user-domain/src/test/java/com/ongi/OngiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ongi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OngiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
